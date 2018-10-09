15fb78811f8cd126f0a25afe57a0fa79d324b588
Fate_Zero <fate_zero@Fate-ZerodeMac-mini.local>
2018-10-09 14:55:22 +0800

    翻硬币 限制投注金额

---
+++ limit/limit.cpp
@@ -0,0 +1,20 @@
+#include <eosiolib/eosio.hpp>
+#include "./eosio.token.hpp"
+
+using namespace eosio;
+
+extern "C" {
+    /// The apply method implements the dispatch of events to this contract
+    void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
+      if( code == N(eosio.token) && action == N(transfer) ) {
+        eosio::token t(N(eosio.token));
+        const auto sym_name = eosio::symbol_type(S(4,EOS)).name();
+        uint64_t my_balance = t.get_balance(receiver, sym_name).amount;
+        uint64_t max_amount = my_balance * 0.01;
+
+        auto transfer_args = unpack_action_data<eosio::token::transfer_args>();
+        uint64_t transfer_amount = transfer_args.quantity.amount;
+        eosio_assert( transfer_amount <= max_amount, "Your transfer amount is too large" );
+      }
+   }
+}
