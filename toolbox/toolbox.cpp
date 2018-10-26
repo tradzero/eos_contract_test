9c1ac249f184eacfebfb6832a536b44261cb3a13
Fate_Zero <fate_zero@Fate-ZerodeMac-mini.local>
2018-10-26 11:15:01 +0800

    promotebatch

--- toolbox/toolbox.cpp
+++ toolbox/toolbox.cpp
@@ -1,4 +1,5 @@
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/serialize.hpp>
 #include <string>
 
 using namespace eosio;
@@ -13,6 +14,15 @@ class toolbox : public eosio::contract {
             eosio_assert( is_account( to ), "to account does not exist");
             require_recipient(to);
         }
+
+        [[eosio::action]]
+        void promotebatch( std::vector<account_name>& users, string memo ) {
+            for( size_t i = 0; i < users.size(); i++ ) {
+                if (is_account(users[i])) {
+                    require_recipient(users[i]);
+                }
+            }
+        }
 };
 
-EOSIO_ABI( toolbox, (promote) )
+EOSIO_ABI( toolbox, (promote) (promotebatch))
